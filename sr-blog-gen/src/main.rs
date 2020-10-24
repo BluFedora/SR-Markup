ab14977259188653f2191e5dabae89c9b1b2ad5c
Shareef Raheem <r.shareef28@gmail.com>
2020-10-24 05:59:28 -0700

    Simplied C API and Dynamic Library loading frontend
    ~2hrs
    - Simplified the CPI header genration to just allow reading of an in memory view of the AST.
    - Dynamic Library loading frontend mostly finished.

--- sr-blog-gen/src/main.rs
+++ sr-blog-gen/src/main.rs
@@ -12,14 +12,14 @@
 
 pub mod sr;
 
-use sr::ast_processor::*;
 use sr::ast::*;
-use sr::lexer::*;
+use sr::ast_processor::*;
 use sr::c_api::*;
+use sr::lexer::*;
 
 use structopt::StructOpt; // [https://docs.rs/structopt/0.3.20/structopt/]
 extern crate libloading; // [https://docs.rs/libloading/0.5.0/libloading/] [https://doc.rust-lang.org/nomicon/ffi.html]
-use libc::{c_char, c_int, c_void, size_t};
+use libc::{c_char, c_void};
 
 use std::path::PathBuf;
 
@@ -34,100 +34,52 @@ struct Options {
   #[structopt(short, long)]
   pub quiet: bool,
 
-  #[structopt(short = "l", long = "library", parse(from_os_str))]
+  #[structopt(short = "l", long = "library", parse(from_os_str), default_value = "")]
   pub dll_processor: PathBuf,
 }
 
-// Dynamic Libraries
-
-struct DynamicLibProcessor {
-  api: BlogAPI,
-}
-
-#[allow(non_camel_case_types)]
-type bfPlatformAllocator =
-  fn(ptr: *mut c_void, old_size: size_t, new_size: size_t, user_data: *mut c_void) -> *mut c_void;
-
-#[repr(C)]
-#[allow(non_camel_case_types)]
-struct bfPlatformInitParams {
-  argc: c_int,
-  argv: *mut *mut c_char,
-  allocator: *const bfPlatformAllocator,
-  user_data: *mut c_void,
-}
-
 fn main() {
-  println!();
-
-  let cwd = std::env::current_dir();
-  let args = std::env::args();
-
-  println!("Running in {:?}", cwd.unwrap());
-
-  for arg in args.enumerate() {
-    println!("Arg({}) = {}", arg.0, arg.1);
-  }
-
   let options = Options::from_args();
 
-  println!();
-
-  println!("----------------------------------");
-  println!("----- SR Blog Post Generator -----");
-  println!("----------------------------------\n");
-
-  println!("Options: {:?}", options);
-
-  let dll_lib = libloading::Library::new(options.dll_processor);
-
-  match dll_lib {
-    Ok(library) => unsafe {
-      #[allow(non_snake_case)]
-      let bfPlatformInit: libloading::Symbol<
-        unsafe extern "C" fn(params: bfPlatformInitParams) -> i32,
-      > = library.get(b"bfPlatformInit").unwrap();
-
-      let params = bfPlatformInitParams {
-        argc: 0,
-        argv: std::ptr::null_mut(),
-        allocator: std::ptr::null(),
-        user_data: std::ptr::null_mut(),
-      };
-
-      bfPlatformInit(params);
-
-      #[allow(non_snake_case)]
-      let bfPlatformCreateWindow: libloading::Symbol<
-        unsafe extern "C" fn(*const c_char, c_int, c_int, u32) -> *mut c_void,
-      > = library.get(b"bfPlatformCreateWindow").unwrap();
-
-      let window_name = std::ffi::CString::new("Hello, world!");
-
-      let window =
-        bfPlatformCreateWindow(window_name.unwrap().as_ptr(), 1280, 720, 1 << 1 | 1 << 2);
-
-      #[allow(non_snake_case)]
-      let bfPlatformDoMainLoop: libloading::Symbol<
-        unsafe extern "C" fn(*mut c_void) -> c_void,
-      > = library.get(b"bfPlatformDoMainLoop").unwrap();
-
-      bfPlatformDoMainLoop(window);
-    },
-    Err(msg) => {
-      panic!(format!("Failed to load library \"{}\".", msg));
+  if !options.quiet {
+    let cwd = std::env::current_dir();
+    let args = std::env::args();
+  
+    println!("\nCWD {:?}:", cwd.unwrap());
+  
+    for arg in args.enumerate() {
+      println!("  Arg({}) = '{}'", arg.0, arg.1);
     }
+
+    println!("----------------------------------");
+    println!("----- SR Blog Post Generator -----");
+    println!("----------------------------------\n");
+
+    // println!("Options: {:?}", options);
   }
+  let source = test_blog_string();
 
-  println!("\n----------------------------------\n");
+  if options.dll_processor.as_os_str().is_empty() {
+    process_file(&mut DebugProcessor { current_indent: 0 }, source);
+  } else {
+    let mut processor = DynamicLibProcessor::new(options.dll_processor);
 
-  let lexer = Lexer::new(test_blog_string());
+    if processor.err.is_some() {
+      eprintln!("[ERROR] Failed to load dynamic lib, {}.", processor.err.unwrap());
+      return;
+    }
+
+    process_file(&mut processor, source);
+  }
+}
+
+fn process_file(processor: &mut dyn IASTProcessor, source: String) {
+  let lexer = Lexer::new(source);
   let mut parser = Parser::new(lexer);
   let syntax_tree = parser.parse();
-  let mut process_blog = DebugProcessor { current_indent: 0 };
 
   match syntax_tree {
-    Some(raw_tree) => raw_tree.visit(&mut process_blog),
+    Some(raw_tree) => raw_tree.visit(processor),
     None => {
       eprintln!("ERRORS:");
 
@@ -137,6 +89,215 @@ fn main() {
     }
   }
 }
+
+// Built-In IASTProcessor(s)
+
+/* Dyn-Lib Processor */
+
+type DynLibInitCallbackFn = unsafe extern "C" fn() -> *mut c_void;
+type DynLibProcessCallbackFn =
+  unsafe extern "C" fn(node: *const ASTNodeView, user_data: *mut c_void) -> bool;
+type DynLibShutdownCallbackFn = unsafe extern "C" fn(user_data: *mut c_void);
+
+struct DynamicLibProcessor {
+  node_view_list_stack: Vec<Vec<ASTNodeView>>,
+  node_view_list_index: i32,
+  node_attribute_storage: Vec<Vec<TagAttributeView>>,
+  library: Option<libloading::Library>,
+  err: Option<std::io::Error>,
+}
+
+impl DynamicLibProcessor {
+  fn new(dll_path: PathBuf) -> Self {
+    let dll_lib = libloading::Library::new(dll_path);
+
+    match dll_lib {
+      Ok(library) => {
+        return Self {
+          node_view_list_stack: Default::default(),
+          node_view_list_index: -1,
+          node_attribute_storage: Default::default(),
+          library: Some(library),
+          err: None,
+        };
+      }
+      Err(msg) => {
+        return Self {
+          node_view_list_stack: Default::default(),
+          node_view_list_index: -1,
+          node_attribute_storage: Default::default(),
+          library: None,
+          err: Some(msg),
+        };
+      }
+    }
+  }
+
+  fn push_list(&mut self) {
+    self.node_view_list_stack.push(Default::default());
+    self.node_view_list_index += 1;
+  }
+
+  fn add_to_list(&mut self, node_data: ASTNodeView) {
+    self.current_list_mut().push(node_data);
+  }
+
+  fn pop_list(&mut self) {
+    self.node_view_list_index -= 1;
+  }
+
+  fn current_list_mut(&mut self) -> &mut Vec<ASTNodeView> {
+    &mut self.node_view_list_stack[self.node_view_list_index as usize]
+  }
+
+  fn current_list(&self) -> &Vec<ASTNodeView> {
+    &self.node_view_list_stack[self.node_view_list_index as usize]
+  }
+
+  fn string_to_view(string_value: &String) -> StringView {
+    let str_start = string_value.as_ptr() as *const c_char;
+
+    StringView {
+      str_start: str_start,
+      str_end: unsafe { str_start.offset(string_value.len() as isize) },
+    }
+  }
+
+  fn literal_to_view(literal_node: &sr::ast::ASTNodeLiteral) -> ASTNodeLiteralValue {
+    return match literal_node {
+      ASTNodeLiteral::Str(s) => ASTNodeLiteralValue::AsStr(DynamicLibProcessor::string_to_view(&s)),
+      ASTNodeLiteral::Float(f) => ASTNodeLiteralValue::AsNumber(*f),
+      ASTNodeLiteral::Bool(b) => ASTNodeLiteralValue::AsBoolean(*b),
+    };
+  }
+
+  fn list_to_view(list: &Vec<ASTNodeView>) -> ASTNodeListView {
+    ASTNodeListView {
+      num_nodes: list.len() as u32,
+      nodes: list.as_ptr(),
+    }
+  }
+
+  fn add_attributes(&mut self, attribs: &std::collections::HashMap<String, ASTNodeLiteral>) {
+    let index = self.node_attribute_storage.len();
+    self.node_attribute_storage.push(Default::default());
+    let attrib_list = &mut self.node_attribute_storage[index];
+
+    for attrib in attribs {
+      attrib_list.push(TagAttributeView {
+        key: DynamicLibProcessor::string_to_view(attrib.0),
+        value: DynamicLibProcessor::literal_to_view(attrib.1),
+      });
+    }
+  }
+
+  fn current_attributes(&self) -> &Vec<TagAttributeView> {
+    &self.node_attribute_storage[self.node_attribute_storage.len() - 1]
+  }
+}
+
+impl IASTProcessor for DynamicLibProcessor {
+  fn has_error(&mut self) -> bool {
+    false
+  }
+
+  fn visit_begin_root(&mut self, _: &sr::ast::ASTNodeRoot) {
+    self.push_list();
+  }
+
+  fn visit_begin_tag(&mut self, _: &sr::ast::ASTNodeTag) {
+    self.push_list();
+  }
+
+  fn visit_text(&mut self, text_node: &sr::ast::ASTNodeText) {
+    self.add_to_list(ASTNodeView::Text(DynamicLibProcessor::string_to_view(
+      &text_node.text,
+    )));
+  }
+
+  fn visit_literal(&mut self, literal_node: &sr::ast::ASTNodeLiteral) {
+    self.add_to_list(ASTNodeView::Literal(DynamicLibProcessor::literal_to_view(
+      literal_node,
+    )));
+  }
+
+  fn visit_end_tag(&mut self, tag_node: &sr::ast::ASTNodeTag) {
+    self.pop_list();
+
+    self.add_attributes(&tag_node.attributes);
+
+    let attrib_list = self.current_attributes();
+    let attrib_list_len = attrib_list.len() as u32;
+    let attrib_list_ptr = attrib_list.as_ptr();
+
+    self.add_to_list(ASTNodeView::Tag(
+      DynamicLibProcessor::string_to_view(&tag_node.text),
+      DynamicLibProcessor::list_to_view(&self.current_list()),
+      attrib_list_len,
+      attrib_list_ptr,
+    ))
+  }
+
+  fn visit_end_root(&mut self, _: &sr::ast::ASTNodeRoot) {
+    unsafe {
+      self.pop_list();
+
+      let mut user_data: *mut c_void = std::ptr::null_mut();
+      let library = self.library.as_ref().unwrap();
+
+      // Init
+      {
+        let init_cb_res: libloading::Result<libloading::Symbol<DynLibInitCallbackFn>> =
+          library.get(b"srBlogGenInit");
+
+        match init_cb_res {
+          Ok(init_cb) => {
+            user_data = init_cb();
+          }
+          Err(_err) => {
+            eprint!("[WARN]: failed to find a 'srBlogGenInit' callback.");
+          }
+        }
+      }
+
+      // Process
+      {
+        let process_cb_res: libloading::Result<libloading::Symbol<DynLibProcessCallbackFn>> =
+          library.get(b"srBlogGenProcess");
+
+        match process_cb_res {
+          Ok(process_cb) => {
+            let children = self.current_list();
+
+            for node in children {
+              process_cb(&*node, user_data);
+            }
+          }
+          Err(_err) => {
+            eprint!("[WARN]: failed to find a 'srBlogGenProcess' callback.");
+          }
+        }
+      }
+
+      // Shutdown
+      {
+        let shutdown_cb_res: libloading::Result<libloading::Symbol<DynLibShutdownCallbackFn>> =
+          library.get(b"srBlogGenShutdown");
+
+        match shutdown_cb_res {
+          Ok(shutdown_cb) => {
+            shutdown_cb(user_data);
+          }
+          Err(_err) => {
+            eprint!("[WARN]: failed to find a 'srBlogGenShutdown' callback.");
+          }
+        }
+      }
+    }
+  }
+}
+
+/* Debug-Processor */
 
 struct DebugProcessor {
   current_indent: u32,
--- sr-blog-gen/src/sr/c_api.rs
+++ sr-blog-gen/src/sr/c_api.rs
@@ -4,25 +4,18 @@
 
 // Dynamic Library API
 
-// GetType() -> c_int
-// (ASTNodeRoot, ASTNodeTag) Child Begin, Child Next, Child End / HasNext
-// (ASTNodeTag, ASTNodeText) GetText
-
-use crate::sr::ast::ASTNode;
-// use crate::sr::ast::ASTNodeList;
-
-use libc::{c_char, c_int, c_void, size_t};
+use libc::{c_char};
 
 #[repr(C)]
 pub struct StringView {
-  str_start: *const c_char,
-  str_end: *const c_char,
+  pub str_start: *const c_char,
+  pub str_end: *const c_char,
 }
 
 #[repr(C)]
 pub struct ASTNodeListView {
-  num_nodes: size_t,
-  nodes: *const ASTNodeView,
+  pub num_nodes: u32,
+  pub nodes: *const ASTNodeView,
 }
 
 #[repr(C)]
@@ -37,35 +30,33 @@ pub enum ASTNodeLiteralValue {
 
 #[repr(C)]
 pub struct TagAttributeView {
-  key: StringView,
-  value: ASTNodeLiteralValue,
+  pub key: StringView,
+  pub value: ASTNodeLiteralValue,
 }
 
 #[repr(C)]
 /// cbindgen:prefix-with-name
-pub enum ASTNodeView
-{
-  /// cbindgen:field-names=[children]
-  Root(ASTNodeListView),
+pub enum ASTNodeView {
   /// cbindgen:field-names=[text, children, num_attributes, attributes]
-  Tag(StringView, ASTNodeListView, size_t, * const TagAttributeView),
+  Tag(StringView, ASTNodeListView, u32, *const TagAttributeView),
   /// cbindgen:field-names=[text]
   Text(StringView),
   /// cbindgen:field-names=[value]
   Literal(ASTNodeLiteralValue),
 }
 
+/*
 #[repr(C)]
 pub struct BlogAPI {
   user_data: *mut c_void,
-  attribute_begin: extern "C" fn(node: *mut c_int),
-  attribute_has_next: extern "C" fn(node: *mut c_int) -> bool,
-  attribute_next: extern "C" fn(node: *mut c_int),
-  get_text: extern "C" fn(node: *mut c_int),
+  attribute_begin: extern "C" fn(node: *mut c_void),
+  attribute_has_next: extern "C" fn(node: *mut c_void) -> bool,
+  attribute_next: extern "C" fn(node: *mut c_void),
+  get_text: extern "C" fn(node: *mut c_void),
 }
 
 #[no_mangle]
-pub extern "C" fn srBlogNumChildren(node_ptr: *const c_void) -> size_t {
+pub extern "C" fn srBlogNumChildren(node_ptr: *const c_void) -> u32 {
   let node: *const ASTNode = node_ptr as *const ASTNode;
 
   unsafe {
@@ -84,8 +75,10 @@ pub extern "C" fn srBlogChildAt(node_ptr: *const c_void, index: size_t) -> *cons
 
   unsafe {
     return match *node {
-      ASTNode::Root(ref node_root) => &*node_root.children[index] as *const ASTNode as *const c_void,
-      ASTNode::Tag(ref node_tag) =>  &*node_tag.children[index]as *const ASTNode as *const c_void,
+      ASTNode::Root(ref node_root) => {
+        &*node_root.children[index] as *const ASTNode as *const c_void
+      }
+      ASTNode::Tag(ref node_tag) => &*node_tag.children[index] as *const ASTNode as *const c_void,
       ASTNode::Text(_) => std::ptr::null(),
       ASTNode::Literal(_) => std::ptr::null(),
     };
@@ -93,7 +86,7 @@ pub extern "C" fn srBlogChildAt(node_ptr: *const c_void, index: size_t) -> *cons
 }
 
 #[no_mangle]
-pub extern "C" fn srBlogChildrenHasNext(this: *const c_void) -> c_int {
+pub extern "C" fn srBlogChildrenNext(this: *const c_void) -> c_void {
   if this == std::ptr::null() {
     return 1;
   }
@@ -102,18 +95,10 @@ pub extern "C" fn srBlogChildrenHasNext(this: *const c_void) -> c_int {
 }
 
 #[no_mangle]
-pub extern "C" fn srBlogChildrenNext(this: *const c_void) -> c_int {
-  if this == std::ptr::null() {
-    return 1;
-  }
-
-  return 0;
-}
-
-#[no_mangle]
-pub extern "C" fn srBlogGetText() -> StringView {
+pub extern "C" fn srBlogGetText(this: *const c_void) -> StringView {
   return StringView {
     str_start: std::ptr::null(),
     str_end: std::ptr::null(),
   };
 }
+*/
